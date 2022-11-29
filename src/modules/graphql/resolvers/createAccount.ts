19a5ec3c77567e814323b0725d1e489922e48f98
Niks Kozlovs <niks.kozlovs@chi.lv>
2022-11-29 22:11:48 +0200

    Added simple registration

--- src/modules/graphql/resolvers/createAccount.ts
+++ src/modules/graphql/resolvers/createAccount.ts
@@ -1,17 +1,34 @@
+import { hash } from "bcrypt";
 import { FieldResolver } from "nexus";
 
 export const createAccount: FieldResolver<
   "Mutation",
   "createAccount"
-> = async (_, { credentials }) => {
+> = async (_, { credentials }, { prisma }) => {
   try {
+    const existingUser = await prisma.user.findUnique({ where: { email: credentials.email } });
+
+    if (existingUser !== null) {
+      throw new Error("User with this email already exists!");
+    }
+
+    const hashedPassword = await hash(credentials.password, 7);
+
+    await prisma.user.create({
+      data: {
+        username: credentials.username,
+        email: credentials.email,
+        passhash: hashedPassword,
+      }
+    });
+
     return {
       message:
         "Thanks for registering! Check your email to validate your account.",
       error: false,
     };
   } catch (err) {
-    const message = "Invalid Input";
+    const message = err as string || "Invalid Input";
     return {
       message,
       error: true,
