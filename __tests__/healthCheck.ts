5a074553835a956e357267556a131e310db8e1f6
Niks Kozlovs <niks.kozlovs@chi.lv>
2022-12-13 21:07:48 +0200

    Added jest for testing

---
+++ __tests__/healthCheck.ts
@@ -0,0 +1,17 @@
+import { gql } from "@apollo/client";
+import { server } from "../src/lib/apolloServer";
+
+it("Runs a health check agains graphql schema", async () => {
+  const result = await server.executeOperation({
+    query: gql`
+      query Query {
+        ok
+      }
+    `,
+  });
+
+  expect(result).toBeTruthy();
+  expect(result).toHaveProperty("data");
+  expect(result.errors).toBeFalsy();
+  expect(result.data?.ok).toEqual(true);
+});
